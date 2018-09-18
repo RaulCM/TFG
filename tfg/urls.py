9ce62c36f6db976a4632471bd74936af0295021c
RaulCM <raulcanomontero@hotmail.com>
2018-09-18 19:19:59 +0200

    Modificaciones para añadir la app

--- tfg/urls.py
+++ tfg/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from analyzerapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('analyzerapp.urls')),
+    #url(r'^$', views.holamundo, name='holamundo'),
 ]
