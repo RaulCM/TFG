ea1c1afafa8a58e172aea2c7f654802afc4b9512
RaulCM <raulcanomontero@hotmail.com>
2018-09-19 18:43:19 +0200

    Creada view githubSearch

--- analyzerapp/views.py
+++ analyzerapp/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from analyzerapp.models import Repository
+import requests, json
+import os
 # Create your views here.
 
 def holamundo(request):
     return HttpResponse("Hola Mundo")
+
+def githubSearch(arg):
+
+    url = 'https://api.github.com/search/repositories'
+    #https://developer.github.com/v3/search/#search-repositories
+    #https://help.github.com/articles/understanding-the-search-syntax/
+    queries = '?q=python3'      #Que contengan el string "python3"
+    queries += '+language:python'    #Solo lenguaje Python
+    queries += '+archived:false'    #Repositorios no archivados
+    queries += '+created:>2018-06-01'    #Fecha posterior a YYYY:MM:DD
+    queries += '+topics:>3'            #Numero de topics mayor a X
+    queries += '&sort=updated'        #Ordenados por fecha de actualizacion
+
+    url = url + queries
+    r = requests.get(url)
+
+    json_data = r.json()
+    json_data = json_data["items"]
+    for item in json_data:
+        repository = Repository()
+        repository.identifier = item["id"]
+        repository.full_name = item["full_name"]
+        if item["description"] is not None:
+            repository.description = item["description"]
+        repository.html_url = item["html_url"]
+        repository.save()
+
+
+    json_pretty = json.dumps(json_data, sort_keys=True, indent=4)
+
+
+    #pprint.pprint(r.json())
+    #return HttpResponse(json_pretty,content_type="application/json")
+
+    return HttpResponse(json_pretty,content_type="text/json")
