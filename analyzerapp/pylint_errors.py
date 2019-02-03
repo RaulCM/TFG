8d6daccdc707b57e4525350543ea0e60cf93453f
RaulCM <raulcanomontero@hotmail.com>
2019-02-03 12:05:01 +0100

    Corrección error C0303

--- analyzerapp/pylint_errors.py
+++ analyzerapp/pylint_errors.py
@@ -5,13 +5,28 @@ class Error:
         self.code = data[2].rstrip()
 
 def check(error):
-    if error.code == 'C0326':
-        c0326(error)
+    if error.code == 'C0303':
+        c0303(error)
+        print('C0303')
+    # elif error.code == 'C0326':
+    #     c0326(error)
     elif error.code == 'R0201':
         print("OK")
     else:
         print("NO");
 
+def replace_lines(file, lines):
+    fo = open(file, 'w')
+    fo.writelines(lines)
+    fo.close()
+
+def c0303(error):
+    # Trailing whitespace
+    fo = open(error.path, "r")
+    lines = fo.readlines()
+    fo.close()
+    lines[error.line] = lines[error.line].rstrip() + "\n"
+    replace_lines(error.path, lines)
 
 def c0326(error):
     fo = open(error.path, "r+")
