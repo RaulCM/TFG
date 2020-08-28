5906ff8dc13f100ba7a9fda6fec1bc0b8f1f48ba
RaulCM <raulcanomontero@hotmail.com>
2020-08-28 14:01:51 +0200

    pylint_errors.py: C0326 modificada

--- analyzerapp/pylint_errors.py
+++ analyzerapp/pylint_errors.py
@@ -12,12 +12,12 @@ def check(error):
     if error.code == 'C0303':
         c0303(error)
         print('C0303')
-    elif error.code == 'C0326':
-        # c0326(error)
-        print("C0326")
     elif error.code == 'C0321':
         c0321(error)
         print("C0321")
+    elif error.code == 'C0326':
+        c0326(error)
+        print("C0326")
     elif error.code == 'C0410':
         c0410(error)
         print("C0410")
@@ -105,16 +105,56 @@ def c0321(error):
     lines[error.line] = "#SPLIT" + str(error.column) + "#SPLIT" + lines[error.line]
     replace_lines(error.path, lines)
 
+def replace_comma(line, old, new):
+    line = line.split(old)
+    new_line = line[0].rstrip()
+    i = 1
+    while i < len(line):
+        new_line = new_line + new + line[i].strip()
+        i = i + 1
+    new_line = new_line + "\n"
+    return new_line
+
+def replace_bracket1(line, old, new):
+    new_line = line
+    line = line.split(old, 1)
+    if len(line) > 1:
+        new_line = line[0] + new + line[1].strip() + "\n"
+    return new_line
+
+def replace_bracket2(line, old, new):
+    new_line = line
+    line = line.split(old, 1)
+    if len(line) > 1:
+        new_line = line[0].rstrip() + new + line[1].rstrip() + "\n"
+    return new_line
+
 def c0326(error):
     # %s space %s %s %s\n%s
-    fo = open(error.path, "r+")
-    line = fo.readlines()[error.line]
-    print(line)
-    pattern = r'.*[^=!<>+\-*/&|^% ]=[^=!<>+\-*/&|^% ].*'
-    new = re.search(pattern, line)
-    print(new)
-    # TODO Sustituir linea
-    fo.close()
+    lines = read_file(error)
+    line = lines[error.line]
+    if (error.msg == "Exactly one space required around assignment" or
+        error.msg == "Exactly one space required after assignment" or
+        error.msg == "Exactly one space required before assignment"):
+        line = line.split("=",1)
+        lines[error.line] = line[0].rstrip() + " = " + line[1].lstrip()
+        replace_lines(error.path, lines)
+    elif (error.msg == "Exactly one space required after comma" or
+          error.msg == "No space allowed before comma"):
+        lines[error.line] = replace_comma(line, ",", ", ")
+        replace_lines(error.path, lines)
+    elif error.msg == "No space allowed after bracket":
+        line = replace_bracket1(line, "( ", "(")
+        line = replace_bracket1(line, "[ ", "[")
+        line = replace_bracket1(line, "{ ", "{")
+        lines[error.line] = line
+        replace_lines(error.path, lines)
+    elif error.msg == "No space allowed before bracket":
+        line = replace_bracket2(line, " )", ")")
+        line = replace_bracket2(line, " ]", "]")
+        line = replace_bracket2(line, " }", "}")
+        lines[error.line] = line
+        replace_lines(error.path, lines)
 
 def c0410(error):
     # Multiple imports on one line (%s)
