d4501964e67a9b8f66c24dbc079638e9bc8110fa
RaulCM <raulcanomontero@hotmail.com>
2020-09-03 13:19:50 +0200

    test.py: Creado test_w0404

--- analyzerapp/tests.py
+++ analyzerapp/tests.py
@@ -12,10 +12,6 @@ class PylintErrorsTestCase(TestCase):
         """'More than one statement in a single line' marcados correctamente"""
         self.assertEqual(pylint_errors.c0321(["x = 5;y = 6\n"], 0, 6), ["#SPLIT6#SPLITx = 5;y = 6\n"])
 
-    def test_placeholder_split(self):
-        """Placeholder #SPLIT eliminado y línea corregida correctamente"""
-        self.assertEqual(pylint_errors.placeholder_split("#SPLIT6#SPLITx = 5;y = 6\n"), "x = 5\ny = 6\n")
-
     def test_c0326(self):
         msg = "Exactly one space required around assignment"
         self.assertEqual(pylint_errors.c0326(["x=5\n"], 0, msg), ["x = 5\n"])
@@ -34,10 +30,6 @@ class PylintErrorsTestCase(TestCase):
         """'Multiple imports in one line' marcados correctamente"""
         self.assertEqual(pylint_errors.c0410(["import os, re\n"], 0), ["#IMPORTSPLITimport os, re\n"])
 
-    def test_placeholder_importsplit(self):
-        """Placeholder #IMPORTSPLIT eliminado y línea corregida correctamente"""
-        self.assertEqual(pylint_errors.placeholder_importsplit("#IMPORTSPLITimport os, re\n"), "import os\nimport re\n")
-
     def test_c0411(self):
         """'wrong-import-order: %s comes before %s' marcados correctamente"""
         msg = 'standard import "import os" comes before "import requests"\n'
@@ -45,18 +37,30 @@ class PylintErrorsTestCase(TestCase):
         msg = 'external import "import requests" comes before "from fork import USERNAME"\n'
         self.assertEqual(pylint_errors.c0411(["import os\n", "from fork import USERNAME\n", "import requests\n"], 2, msg), ["import os\n", "from fork import USERNAME\n", "#EXT1#EXTimport requests\n"])
 
-    def test_placeholder_external(self):
-        """Placeholder #EXT eliminado y línea corregida correctamente"""
-        self.assertEqual(pylint_errors.placeholder_external(["import os\n", "from fork import USERNAME\n", "#EXT1#EXTimport requests\n"], 2), ["import os\n", "import requests\n", "from fork import USERNAME\n"])
-
     def test_c0413(self):
         """'Import should be placed at the top of the module' marcados correctamente"""
         self.assertEqual(pylint_errors.c0413(["x = 5", "import os", "y = 6"], 1), ["x = 5", "#TOPimport os", "y = 6"])
 
-    def test_placeholder_top(self):
-        """Placeholder #TOP eliminado y línea corregida correctamente"""
-        self.assertEqual(pylint_errors.placeholder_top(["x = 5", "#TOPimport os", "y = 6"], 1), ["import os", "x = 5", "y = 6"])
+    def test_w0404(self):
+        """'Unused import' marcado correctamente"""
+        self.assertEqual(pylint_errors.w0404(["import os\n", "import os\n", "import requests\n"], 1), ["import os\n", "#DELimport os\n", "import requests\n"])
 
     def test_w0611(self):
         """'Unused import' marcado correctamente"""
         self.assertEqual(pylint_errors.w0611(["import os\n"], 0), ["#DELimport os\n"])
+
+    def test_placeholder_split(self):
+        """Placeholder #SPLIT eliminado y línea corregida correctamente"""
+        self.assertEqual(pylint_errors.placeholder_split("#SPLIT6#SPLITx = 5;y = 6\n"), "x = 5\ny = 6\n")
+
+    def test_placeholder_importsplit(self):
+        """Placeholder #IMPORTSPLIT eliminado y línea corregida correctamente"""
+        self.assertEqual(pylint_errors.placeholder_importsplit("#IMPORTSPLITimport os, re\n"), "import os\nimport re\n")
+
+    def test_placeholder_external(self):
+        """Placeholder #EXT eliminado y línea corregida correctamente"""
+        self.assertEqual(pylint_errors.placeholder_external(["import os\n", "from fork import USERNAME\n", "#EXT1#EXTimport requests\n"], 2), ["import os\n", "import requests\n", "from fork import USERNAME\n"])
+
+    def test_placeholder_top(self):
+        """Placeholder #TOP eliminado y línea corregida correctamente"""
+        self.assertEqual(pylint_errors.placeholder_top(["x = 5", "#TOPimport os", "y = 6"], 1), ["import os", "x = 5", "y = 6"])
