e903cd56e390ff1a2cae91521c28dc0255b4934b
RaulCM <raulcanomontero@hotmail.com>
2021-06-25 10:32:53 +0200

    Cambiada tabulación

--- analyzerapp/urls.py
+++ analyzerapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main),
-    url(r'^es$', views.es),
-    url(r'^en$', views.en),
+	url(r'^es$', views.es),
+	url(r'^en$', views.en),
 	url(r'^list$', views.list),
 	url(r'^error-list$', views.error_list),
 	url(r'^guide$', views.guide),
