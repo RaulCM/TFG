19093d3aa1302ca1769293d7e693d0989fc1c0e9
RaulCM <raulcanomontero@hotmail.com>
2021-06-25 10:19:17 +0200

    Añadidas urls de cambio de idioma

--- analyzerapp/urls.py
+++ analyzerapp/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main),
-	url(r'^list$', views.list),
-	url(r'^error-list$', views.error_list),
-	url(r'^guide$', views.guide),
-	url(r'^contact$', views.contact),
-	url(r'^search$', views.github_search),
-	url(r'^repo/(\d+)$', views.repo),
+    url(r'^list$', views.list),
+    url(r'^error-list$', views.error_list),
+    url(r'^guide$', views.guide),
+    url(r'^contact$', views.contact),
+    url(r'^search$', views.github_search),
+    url(r'^en$', views.en),
+    url(r'^es$', views.es),
+    url(r'^repo/(\d+)$', views.repo),
     #url(r'^pylint$', views.runPylint),
 ]
